d6246bc9863b0e262e6c3f7bd9b65dfd2937bea9
sunjisu1 <31647896+sunjisu1@users.noreply.github.com>
2020-10-15 20:56:56 +0900

    2주차 플레이어 컴포넌트

---
+++ CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    // 카메라 관련 컴포넌트
+    [SerializeField] private Camera theCamera; // 카메라 세팅
+    [SerializeField] private float cameraMoving; // 카메라 움직임
+    [SerializeField] private float cameraLimit; // 카메라 움직임 제한
+
+    private float currentCameraRotateX; // 카메라 상하 회전
+
+   
+    private void FixedUpdate()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float rotationX = Input.GetAxisRaw("Mouse Y"); // 상하 이동
+        float cameraRotationX = rotationX * cameraMoving; // 카메라 상하 조절
+
+        currentCameraRotateX -= cameraRotationX;
+        currentCameraRotateX = Mathf.Clamp(currentCameraRotateX, -cameraLimit, cameraLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotateX, 0f, 0f);
+    }
+
+}
---
+++ PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    // 플레이어 컴포넌트
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float playerRotateLimit; // 캐릭터 회전 제한
+
+    private Rigidbody myRigid; 
+
+
+	// Use this for initialization
+	void Start () {
+        myRigid = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Move();
+        PlayerRotation();
+    }
+
+    private void PlayerRotation()
+    {
+        float rotationY = Input.GetAxisRaw("Mouse X");
+
+        Vector3 playerRotationY = new Vector3(0f, rotationY, 0f) * playerRotateLimit;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(playerRotationY));
+    }
+
+    private void Move()
+    {
+        float moveDirX = Input.GetAxisRaw("Horizontal"); // 좌우 이동
+        float moveDirZ = Input.GetAxisRaw("Vertical"); // 상하 이동
+
+        Vector3 moveHorzontal = transform.right * moveDirX;
+        Vector3 moveVertical = transform.forward * moveDirZ;
+
+        Vector3 movePos = (moveHorzontal + moveVertical).normalized * walkSpeed; // 움직임 체크
+
+        myRigid.MovePosition(transform.position + movePos * Time.deltaTime); 
+    }
+
+
+}
